3791281bdc59ca880d3cd2e375ebc4105e9cab6f
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-11-27 21:50:39 +0530

    [scale.concurrency] Initial Commit. Adding queue.js as a submodule. TODO add queues.js as dep.  [queues.js] Initial commit. TODO add test and demo files. Installing 0.0.2-beta file. Removing other deps. Minor changes to file docs

---
+++ demos/demos.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Package: scale.concurrency
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i scale.concurrency --save
+ * Github: https://github.com/ganeshkbhat/scale.concurrency
+ * npmjs Link: https://www.npmjs.com/package/scale.concurrency
+ * File: .js
+ * File Description: .js 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
